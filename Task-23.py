ce72413dadb4238039d83c818e06d507d837b182
RJPriyadharshini <142367764+RJPriyadharshini@users.noreply.github.com>
2024-01-12 09:55:47 +0530

    Update Task-23.py

--- Task-23.py
+++ Task-23.py
@@ -7,21 +7,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 #create webdriver object
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
-#oepn the url
+#open the url
 driver.get("https://jqueryui.com/droppable/")
-
-# Implicit wait is applicable to all the statements below
-f1=driver.find_element(By.XPATH,"//iframe[@class='demo-frame']")
-# Switched to frame 1
-driver.switch_to.frame(f1)
+frame_id=driver.find_element(By.XPATH,"//iframe[@class='demo-frame']")
+# Switched to frame
+driver.switch_to.frame(frame_id)
 #get source element
 source_element = driver.find_element(By.XPATH, "//div[@id='draggable']")
 #get target element
 target_element = driver.find_element(By.XPATH, "//div[@id='droppable']")
-#create action chain object
+#create action chain object and stored in one variable
 act = ActionChains(driver)
-#perform the action
+# perform the action
 act.drag_and_drop(source_element, target_element).perform()
 time.sleep(10)
 driver.quit()
