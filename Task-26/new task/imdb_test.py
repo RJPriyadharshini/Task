787eb05a1526ac0f0145f4874b859371d01a9cac
RJPriyadharshini <142367764+RJPriyadharshini@users.noreply.github.com>
2024-02-21 00:25:34 +0530

    Update imdb_test.py

--- Task-26/new task/imdb_test.py
+++ Task-26/new task/imdb_test.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 import time
 
 from IMBd.pom.pom_file import InputFieldPage
+
 def test_enter_name(browser):
     name_field = InputFieldPage(browser)
     name_field.SetNameInput("Pavi")
-
 
 def test_enter_birthdate(browser):
     birthdate_field = InputFieldPage(browser)
@@ -20,11 +20,9 @@ def test_select_awards(browser):
     awards_field = InputFieldPage(browser)
     awards_field.SelectAwards()
 
-
 def test_select_pagetopics(browser):
     pagetopics_field = InputFieldPage(browser)
     pagetopics_field.SelectPageTopic("Quotes")
-
 
 def test_select_gender(browser):
     gender_field = InputFieldPage(browser)
