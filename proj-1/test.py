a19105419ea60da65ffacaad9212aeadb0876b74
RJPriyadharshini <142367764+RJPriyadharshini@users.noreply.github.com>
2024-03-29 15:00:37 +0530

    Update test.py

--- proj-1/test.py
+++ proj-1/test.py
@@ -2,60 +2,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-
-#import the page objects from pom file
+# import the page objects from pom file
 from pom import OrangeHRM
-error_text="//div[@role='alert']"
 
-#testcase_01 valid login
+error_text = "//div[@role='alert']"
+
+
 try:
-   def test1_valid_login(browser):
-   
+         # testcase_01 valid login
+    def test1_valid_login(browser):
         login_page = OrangeHRM(browser)
         login_page.valid_login("Admin", "admin123")
-        if "dashboard"in browser.current_url:
-             print("login successful")
+        if "dashboard" in browser.current_url:
+            print("login successful")
         else:
-             print("login not successful")
-    
+            print("login not successful")
 
-#testcase_02 invalid login
 
-   def test2_invalid_login(browser):
+        # testcase_02 invalid login
+    def test2_invalid_login(browser):
         login_page = OrangeHRM(browser)
         login_page.invalid_login("priya", "admin1")
-        error_message =WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, error_text)))
+        error_message = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, error_text)))
         print("invalid login error message:", error_message.text)
         assert error_message.text == "Invalid credentials", "Error message is not displayed correctly
 
-#testcase_3 add new employee
-
+        # testcase_3 add new employee
     def test3_add_employee(browser):
-    
-        login_page = OrangeHRM(browser)
-        login_page.valid_login("Admin", "admin123")
-        add_user =OrangeHRM(browser)
-        add_user.add_employee("priya", "data", "G","123","3434","2024-04-11","2000-07-07","add employee","employee added")
-        print("Employee added successfully")
-  
-#testcase_4 edit the existing employee
 
-    def test4_edit_employee(browser):
-    #login the application
         login_page = OrangeHRM(browser)
         login_page.valid_login("Admin", "admin123")
         add_user = OrangeHRM(browser)
-        add_user.edit_employee("dharshan","G","4343","98","2022-09-09")
-        print("Employee updated successfully")
-     
+        add_user.add_employee("priya", "data", "G", "123", "3434", "2024-04-11", "2000-07-07", "add employee",
+                              "employee added")
+        print("Employee added successfully")
 
-#testcase_5 delete the employee
+        # testcase_4 edit the existing employee
+    def test4_edit_employee(browser):
+        login_page = OrangeHRM(browser)
+        login_page.valid_login("Admin", "admin123")
+        add_user = OrangeHRM(browser)
+        add_user.edit_employee("dharshan", "G", "4343", "98", "2022-09-09")
+        print("Employee updated successfully")
+
+        # testcase_5 delete the employee
     def test5_del_employee(browser):
-    
-        login_page =OrangeHRM(browser)
-        login_page.valid_login("Admin","admin123")
-        del_user =OrangeHRM(browser)
+
+        login_page = OrangeHRM(browser)
+        login_page.valid_login("Admin", "admin123")
+        del_user = OrangeHRM(browser)
         del_user.del_employee()
         print("employee deleted successfully")
 except Exception as e:
-      print("error message as ", e)
+    print("error message as ", e)
